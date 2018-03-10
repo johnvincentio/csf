ac98005626d908884fa3f8103012910ca5c1096b
johnvincentio <john@johnvincent.io>
2018-03-10 17:19:05 -0500

    shift bits

---
+++ 2.2.4/questions/quadrauples.js
@@ -0,0 +1,24 @@
+/*
+Write a function which quadruples an integer.
+*/
+
+function leftShift(num, shift) {
+	return num << shift;
+}
+
+function exercise(num) {
+	let result = leftShift(num, 2);
+	console.log(`num ${num}, ${num.toString(2)} result ${result}, ${result.toString(2)}`);
+}
+
+let test = 0;
+do {
+	test++;
+	exercise(test);
+} while (test < 10);
+
+let arr1 = [-1000000, -30000, -1, 1, 10, 25, 100, 30000, 60000, 100000000000];
+let arr = [-1, -5, -100, -255, -256];
+arr.map(item => {
+	exercise(item);
+});

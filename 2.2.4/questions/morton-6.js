73462b9ce58e7440b0888f8b1feed633721ac177
johnvincentio <john@johnvincent.io>
2018-03-15 12:53:24 -0400

    fix

--- 2.2.4/questions/morton-6.js
+++ 2.2.4/questions/morton-6.js
@@ -5,18 +5,18 @@ In a Morton Number, the bits of two numbers are interleaved.
 So if your inputs were: 15 (00001111) and 48 (00110000), your output would be 1450 (0000010110101010).
 */
 
-let v15 = 0b00001111;
-let a15 = 0b0000000010101010;
+const v15 = 0b00001111;
+const a15 = 0b0000000010101010;
 //          1234567890123456
 
-let v48 = 0b00110000;
-let a48 = 0b0000010100000000;
+const v48 = 0b00110000;
+const a48 = 0b0000010100000000;
 //          1234567890123456
 
-let actual = 0b0000010110101010;
+const actual = 0b0000010110101010;
 trace("actual ", actual);
 
-let a15_48 = a15 | a48;
+const a15_48 = a15 | a48;
 trace("a15_48 ", a15_48);
 if (a15_48 !== actual) {
 	console.error("ERROR; a15_48 ", a15_48, " should be ", actual);
@@ -52,7 +52,7 @@ function transform(num, move) {
 	let result = 0;
 	for (let i = 1; i < 18; i++) {
 		if (isBit(num, i)) {
-			let j = move ? i * 2 : i * 2 - 1;
+			const j = move ? i * 2 : i * 2 - 1;
 			result = setBit(result, j);
 		}
 	}
@@ -65,7 +65,7 @@ function morton(num1, num2) {
 	return transform(num1, false) | transform(num2, true);
 }
 
-let total = morton(v48, v15);
+const total = morton(v48, v15);
 trace("total ", total);
 if (total !== actual) {
 	console.error("ERROR; total ", total, " should be ", actual);

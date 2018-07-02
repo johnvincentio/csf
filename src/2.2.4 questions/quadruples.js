5365be3f0bb0b18de31c38d8a3205617cf4a145d
johnvincentio <john@johnvincent.io>
2018-07-02 16:48:19 -0400

    add

---
+++ src/2.2.4 questions/quadruples.js
@@ -0,0 +1,21 @@
+//
+
+/* eslint-disable no-plusplus, no-bitwise, no-console, no-restricted-properties */
+
+/*
+Write a function which doubles an integer
+*/
+
+function leftShift(num, shift) {
+	return num << shift;
+}
+
+function quadruples(num) {
+	const result = leftShift(num, 2);
+	console.log(`num ${num} ${num.toString(2)} result ${result}, ${result.toString(2)}`);
+}
+
+const arr = [-1000000, -30000, -1, 1, 0, 1, 2, 10, 25, 100, 255, 256, 30000, 60000, 100000000000];
+arr.map(item => {
+	quadruples(item);
+});

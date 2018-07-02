a30c50895f1f7a90decea891e193a78d31fb2d53
johnvincentio <john@johnvincent.io>
2018-07-02 09:47:06 -0400

    add

--- src/2.1.3 Drills/README.md
+++ src/2.1.3 Drills/README.md
@@ -3,3 +3,9 @@ Complexity: Constant Time complexity
 
 Exercise: Are you here
 Complexity: Data Dependent. Constant Time Complexity to Polynomial Time complexity
+
+Exercise: Doubler
+Complexity: Linear Time Complexity
+
+Exercise: Naive Search
+Complexity: Data Dependent. Linear Time complexity
---
+++ src/2.1.3 Drills/naive-search.js
@@ -0,0 +1,62 @@
+//
+
+/* eslint-disable no-plusplus, no-console, no-param-reassign */
+
+function naiveSearch(array, item) {
+	let ticks = 0;
+	for (let i = 0; i < array.length; i++) {
+		ticks++;
+		if (array[i] === item) {
+			ticks++;
+			return {
+				result: i,
+				ticks
+			};
+		}
+	}
+	return {
+		result: -1,
+		ticks
+	};
+}
+
+function getRunTimeOperations(test, fn, input1, input2) {
+	const { ticks, result } = fn(input1, input2);
+	console.log(
+		`Test ${test}; With input of size ${input1.length} and item ${input2}, ${fn.name} ` +
+			`clocked ${ticks} ticks to generate result of ${result}.`
+	);
+}
+
+let test = 0;
+getRunTimeOperations(++test, naiveSearch, [], 1);
+
+getRunTimeOperations(++test, naiveSearch, [0], 1);
+getRunTimeOperations(++test, naiveSearch, [1], 1);
+getRunTimeOperations(++test, naiveSearch, [1], [2]);
+
+getRunTimeOperations(++test, naiveSearch, [0, 1], 1);
+getRunTimeOperations(++test, naiveSearch, [0, 1], 2);
+
+getRunTimeOperations(++test, naiveSearch, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0);
+
+getRunTimeOperations(
+	++test,
+	naiveSearch,
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+	0
+);
+
+getRunTimeOperations(
+	++test,
+	naiveSearch,
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+	10
+);
+
+getRunTimeOperations(
+	++test,
+	naiveSearch,
+	[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22],
+	50
+);

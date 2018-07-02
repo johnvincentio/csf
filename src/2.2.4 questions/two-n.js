3fe8c6b5f993ab213d7ece5be0ee26659d490716
johnvincentio <john@johnvincent.io>
2018-07-02 16:51:56 -0400

    add

--- src/2.2.4 questions/two-n.js
+++ src/2.2.4 questions/two-n.js
@@ -3,19 +3,19 @@
 /* eslint-disable no-plusplus, no-bitwise, no-console, no-restricted-properties */
 
 /*
-Write a function which quadruples an integer.
+Write a function which divides an integer by two, rounding down.
 */
 
-function leftShift(num, shift) {
-	return num << shift;
+function rightShift(num, shift) {
+	return num >> shift;
 }
 
-function quadruples(num) {
-	const result = leftShift(num, 2);
+function divideByTwo(num) {
+	const result = rightShift(num, 1);
 	console.log(`num ${num} ${num.toString(2)} result ${result}, ${result.toString(2)}`);
 }
 
 const arr = [-1000000, -30000, -1, 1, 0, 1, 2, 10, 25, 100, 255, 256, 30000, 60000, 100000000000];
 arr.map(item => {
-	quadruples(item);
+	divideByTwo(item);
 });

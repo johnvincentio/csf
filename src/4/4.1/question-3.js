9472a423f3493a53fa68b4b07e8a15fe52400808
johnvincentio <john@johnvincent.io>
2018-07-04 17:05:22 -0400

    add

---
+++ src/4/4.1/question-3.js
@@ -0,0 +1,37 @@
+//
+
+/* eslint-disable no-plusplus, no-bitwise, no-console, no-restricted-properties */
+
+/*
+Imagine you are looking for a book in a library with a Dewey Decimal index.
+
+How would you go about it?
+
+Can you express this process as a searching algorithm?
+*/
+
+/*
+Ask the librarian!
+Search library online catalog.
+*/
+
+/*
+It has been a long time since I last looked up a book in a library.
+
+Maybe something like this.
+
+Review Dewey Deciml Classification Categories.
+Determine the category and category number of the book.
+
+I believe there is:
+	an index by author name and 
+	an index by book name.
+
+Don't remember whether these indices were by Category.
+
+With the category, look up the book name index or the author name index.
+
+
+
+
+*/
